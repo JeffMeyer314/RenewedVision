804dbe1a9286f26b03e35d675a9c53c9b8bbe7ca
JeffMeyer314 <43586476+JeffMeyer314@users.noreply.github.com>
2018-09-26 16:20:27 -0400

    1.2.0.0   Change to use TextChanged event
    Syntax Highlighting
      Blue for users
      Red for Hashtags

---
+++ Highlights/Highlights/HighlightField.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Highlights
+{
+    public class HighlightField
+    {
+        public string Text { get; set; }
+        public StateEnum State { get; set; }
+
+        public HighlightField()
+        {
+        }
+
+        public HighlightField(string text, StateEnum state)
+        {
+            Text = text;
+            State = state;
+        }
+
+        List<HighlightField> HighlightFields = new List<HighlightField>();
+        StateEnum CurState;
+        public List<HighlightField> Parse(string x)
+        {
+            CurState = StateEnum.None;
+            StringBuilder sb = new StringBuilder();
+
+            HighlightFields.Clear();
+
+            char prev = ' ';
+            foreach ( char c in x )
+            {
+                if ( Char.IsWhiteSpace(c) )
+                {
+                    switch (CurState)
+                    {
+                        case StateEnum.None:    sb.Append(c); break;
+                        case StateEnum.Normal:  AddHighlightToList(c, sb, StateEnum.Normal, StateEnum.None, addCharBefore: true); break;
+                        case StateEnum.User:    AddHighlightToList(c, sb, StateEnum.User,   StateEnum.None, addCharBefore: false); break;
+                        case StateEnum.Hash:    AddHighlightToList(c, sb, StateEnum.Hash,   StateEnum.None, addCharBefore: false); break;
+                    }
+                }
+                else if (c.Equals('@'))
+                {
+                    bool addBefore = false;
+                    StateEnum newstate = StateEnum.User;
+                    if (!char.IsWhiteSpace(prev))
+                    {
+                        newstate = StateEnum.None;
+                        addBefore = true;
+                    }
+                    switch (CurState)
+                    {
+                        case StateEnum.None:    AddHighlightToList(c, sb, StateEnum.None,   newstate, addBefore); break;
+                        case StateEnum.Normal:  AddHighlightToList(c, sb, StateEnum.Normal, newstate, addBefore); break;
+                        case StateEnum.User:    AddHighlightToList(c, sb, StateEnum.None,   newstate, addBefore); break;
+                        case StateEnum.Hash:    AddHighlightToList(c, sb, StateEnum.None,   newstate, addBefore); break;
+                    }
+                }
+                else if (c.Equals('#'))
+                {
+                    bool addBefore = false;
+                    StateEnum newstate = StateEnum.Hash;
+                    if ( !( char.IsWhiteSpace(prev) || prev.Equals('#') || prev.Equals('@')) )
+                    {
+                        newstate = StateEnum.None;
+                        addBefore = true;
+                    }
+                    switch (CurState)
+                    {
+                        case StateEnum.None:    AddHighlightToList(c, sb, StateEnum.None,   newstate, addBefore); break;
+                        case StateEnum.Normal:  AddHighlightToList(c, sb, StateEnum.Normal, newstate, addBefore); break;
+                        case StateEnum.User:    AddHighlightToList(c, sb, StateEnum.User,   newstate, addBefore); break;
+                        case StateEnum.Hash:    AddHighlightToList(c, sb, StateEnum.None,   newstate, addBefore); break;
+                    }
+                }
+                else if ( char.IsLetterOrDigit(c))
+                {
+                    sb.Append(c);
+                }
+                else // junk chars
+                {
+                    sb.Append(c);                    
+                }
+                prev = c;
+            }
+            AddHighlightToList( sb, CurState, CurState);
+            return HighlightFields;
+        }
+        public void AddHighlightToList(StringBuilder sb, StateEnum addSstate, StateEnum newstate)
+        {
+            StateEnum state = addSstate;
+            if (sb.Length >= 2) sb.Remove(sb.Length - 2, 2);
+            if (sb.Length < 2) state = StateEnum.Normal;
+            if (sb.Length < 1) return;
+
+            HighlightFields.Add(new HighlightField(sb.ToString(), state));
+            sb.Clear();
+
+            CurState = newstate;
+        }
+
+        public void AddHighlightToList(char c, StringBuilder sb, StateEnum addSstate, StateEnum newstate, bool addCharBefore)
+        {
+            if ( addCharBefore ) sb.Append(c);
+            StateEnum state = addSstate;
+            if (sb.Length < 2) state = StateEnum.Normal;
+
+            HighlightFields.Add(new HighlightField(sb.ToString(), state));
+            sb.Clear();
+            if ( ! addCharBefore ) sb.Append(c);
+            CurState = newstate;
+        }
+    }
+
+}
---
+++ Highlights/Highlights/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Highlights
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        public static StateEnum state = StateEnum.Normal;
+        Timer t = new Timer(2000);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            rtb.TextChanged += Rtb_TextChanged;
+        }
+
+
+        string GetString(RichTextBox rtb)
+        {
+            var textRange = new TextRange(rtb.Document.ContentStart, rtb.Document.ContentEnd);
+            return textRange.Text;
+        }
+
+        private void CheckKeyword(RichTextBox rtb)
+        {
+            string msg = GetString(rtb);
+            HighlightField hf = new HighlightField();
+            List<HighlightField> fields = hf.Parse(msg);
+            if (fields.Count > 0)
+            {
+                rtb.Document.Blocks.Clear();
+                Paragraph p = new Paragraph();
+                rtb.Document.Blocks.Add(p);
+
+                foreach (HighlightField curField in fields)
+                {
+                    Run r = new Run(curField.Text);
+                    switch (curField.State)
+                    {
+                        case StateEnum.User: r.Foreground = Brushes.Blue; break;
+                        case StateEnum.Hash: r.Foreground = Brushes.Red; break;
+                        default: r.Foreground = Brushes.Black; break;
+                    }
+                    p.Inlines.Add(r);
+                }
+            }
+        }
+
+        private void Rtb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            rtb.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                TextPointer savePos = SavePosition();
+                rtb.TextChanged -= Rtb_TextChanged;
+                CheckKeyword(rtb);
+                rtb.TextChanged += Rtb_TextChanged;
+                getPos(savePos);
+            }));
+
+        }
+
+        TextPointer SavePosition()
+        {
+            TextPointer tp = rtb.CaretPosition.GetNextInsertionPosition(LogicalDirection.Forward);
+
+            return tp;
+        }
+
+        void getPos(TextPointer tp)
+        {
+            try
+            {
+                rtb.CaretPosition = tp;
+            }
+            catch (Exception exc)
+            {
+                string x = exc.Message;
+                rtb.CaretPosition = rtb.CaretPosition.DocumentEnd;
+            }
+        }
+    }
+}
